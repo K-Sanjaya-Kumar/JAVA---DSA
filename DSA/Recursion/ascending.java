64f3b19b15c5fbe7aec3b8e918116dcc015c12da
K Sanjaya Kumar <kokirisanjaya@gmail.com>
2024-04-07 07:39:41 +0530

    no change

--- DSA/Recursion/ascending.java
+++ DSA/Recursion/ascending.java
@@ -1,14 +1,13 @@
 package DSA.Recursion;
 
 public class ascending {
-    public static void printNumber(int n){
-        if(n==11){
-           // System.out.println(n);
-            return ;
-            
+    public static void printNumber(int n) {
+        if (n == 16) {
+            return;
         }
         System.out.print(n + " ");
         printNumber(n+1);
+
     }
     public static void main(String[] args) {
         int n=1;
