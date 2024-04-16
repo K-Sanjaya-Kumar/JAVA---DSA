641e51d633e676551f5085939ab0b39e4d335161
K Sanjaya Kumar <kokirisanjaya@gmail.com>
2024-04-16 06:56:25 +0530

    natural number sum

---
+++ DSA/Recursion/sumOFn.java
@@ -0,0 +1,22 @@
+package DSA.Recursion;
+
+public class sumOFn {
+    public static void printSum(int i, int n, int sum) {
+        if (i == n) {
+            sum += i;
+            System.out.println(sum);
+            return ;
+        }
+
+        sum+=i;
+        printSum(i+1,n,sum);
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+        int n=5;
+        int i=1;
+        int sum=0;
+        printSum(i, n, sum);
+    }
+}
