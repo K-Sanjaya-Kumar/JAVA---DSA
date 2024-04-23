fdd0f5337a0518fdebe824707f38cf5fbcd1694a
K Sanjaya Kumar <kokirisanjaya@gmail.com>
2024-04-23 08:48:55 +0530

    nothing change

--- DSA/Recursion/sumofNdigit.java
+++ DSA/Recursion/sumofNdigit.java
@@ -6,12 +6,12 @@ public class sumofNdigit {
             return 0;
         }
         int printTotal = printTotal(n / 10) + (n % 10);
-        // System.out.print(printTotal + " ");
+        //  System.out.print(printTotal + " ");
         return printTotal;
     }
 
     public static void main(String[] args) {
-        int n = 123456789;
+        int n = 347344556;
         System.out.println(printTotal(n));
     }
 }
