a63ea6da0111a24668f6f656ac95928ebd8a717d
K Sanjaya Kumar <kokirisanjaya@gmail.com>
2024-04-25 12:17:31 +0530

    x to the power n using O(logn).

--- DSA/Recursion/xpowern1.java
+++ DSA/Recursion/xpowern1.java
@@ -6,10 +6,10 @@ public class xpowern1 {
         if (n == 0) {
             return 1;
         }
-
+        //for Even number
         int halfPower = xPower1(x, n / 2);
         int halfPowersq = halfPower * halfPower;
-
+        //for Odd number
         if (n % 2 == 1) {
             halfPowersq = x * halfPowersq;
         }
