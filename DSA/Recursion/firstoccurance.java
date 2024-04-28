d2acdabb2731629527e0a646951cc04d2e13fdae
K Sanjaya Kumar <kokirisanjaya@gmail.com>
2024-04-28 12:53:04 +0530

    first occurance

---
+++ DSA/Recursion/firstoccurance.java
@@ -0,0 +1,20 @@
+package DSA.Recursion;
+
+public class firstoccurance {
+    public static int firstOccurance(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        if (arr[i] == key) {
+            return i;
+        }
+        return firstOccurance(arr, key, i + 1);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 3, 5, 7, 8, 9 };
+        int key = 9;
+        int i = 0;
+        System.out.println(firstOccurance(arr, key, i));
+    }
+}
