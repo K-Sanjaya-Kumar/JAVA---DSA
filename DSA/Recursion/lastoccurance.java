f1b37280c6b53e719902c4ef0cb0975244ec7bf8
K Sanjaya Kumar <kokirisanjaya@gmail.com>
2024-04-29 09:21:58 +0530

    lastOccurance

---
+++ DSA/Recursion/lastoccurance.java
@@ -0,0 +1,22 @@
+package DSA.Recursion;
+
+public class lastoccurance {
+    public static int lastOccurance(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        int isFound = lastOccurance(arr, key, i + 1);
+
+        if (isFound == -1 && arr[i] == key) {
+            return i;
+        }
+        return isFound;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 8, 3, 6, 9, 5, 10, 2, 5, 3 };
+        int key = 3;
+        int i = 0;
+        System.out.println(lastOccurance(arr, key, i));
+    }
+}
